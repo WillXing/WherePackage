56720ec8fe129092cda10f7fb3bfbf562e8582ee
Xing <yellowstar1992@hotmail.com>
2016-08-16 15:53:37 +0800

    webpack dashboard

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,11 +1,18 @@
 import config from "./webpack.config.babel";
 import webpack from 'webpack';
 import webpackDevServer from 'webpack-dev-server';
+import webpackDashboard from 'webpack-dashboard';
+import webpackDashboardPlugin from 'webpack-dashboard/plugin';
 
-var compiler = webpack(config);
-var server = new webpackDevServer(compiler, {
+let dashboard = new webpackDashboard()
+
+let compiler = webpack(config);
+
+compiler.apply(new webpackDashboardPlugin(dashboard.setData))
+
+let server = new webpackDevServer(compiler, {
 	hot: true,
-	quiet: false,
+	quiet: true,
 	noInfo: false
 });
 
